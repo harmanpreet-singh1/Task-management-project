4f1c3f26a2d2bf380b28f945bdb7af679409fdfe
harman6666 <sharmanpreet@vmware.com>
2024-02-08 22:54:11 +0100

    change folder name and add comment

--- src/hooks/useAppState.ts
+++ src/hooks/useAppState.ts
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { AppStateContext } from '../Contexts';
 
+// Hook for using the App state
 const useAppState = () => useContext(AppStateContext);
 
 export { useAppState };
